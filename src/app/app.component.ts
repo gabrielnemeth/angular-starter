49e08434910b10689faac1c2eb960f029ae0e9e9
gabrielnemeth <github.zaphrentid@aleeas.com>
2022-01-16 19:25:01 +0100

    Set up eslint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {}
 
-  onSubmit(email: string, password: string, evt: Event) {
-    evt.preventDefault();
-    console.log(email, password);
-    this.http.post('/api/auth/login', {email, password}).subscribe(console.log)
-  }
+    onSubmit(email: string, password: string, evt: Event) {
+        evt.preventDefault();
+        console.log(email, password);
+        this.http
+            .post('/api/auth/login', {email, password})
+            .subscribe(console.log);
+    }
 
-  onSignOut() {
-    this.http.get('/api/auth/logout' ).subscribe(console.log)
-  }
+    onSignOut() {
+        this.http.get('/api/auth/logout').subscribe(console.log);
+    }
 }
